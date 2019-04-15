680f2ff516f7aab366afea073bf8be910fba6386
sjloucks <46513109+sjloucks@users.noreply.github.com>
2019-04-14 17:35:59 -0700

    initial set up

--- eclipse-workspace/CSE360GroupProject/src/Status.java
+++ eclipse-workspace/CSE360GroupProject/src/Status.java
@@ -1,4 +1,12 @@
 
 public class Status {
-
+	private int status;
+	private int dateStarted;
+	private int dateFinished;
+	
+	public Status() {
+		status = 1;
+		dateStarted = 0;
+		dateFinished = 0;
+	}
 }
