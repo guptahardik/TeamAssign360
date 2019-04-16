eaf68818771ec00fdde5c60157622e71b8a7099e
BasamAlasaly <bhasaly@gmail.com>
2019-04-15 19:32:39 -0700

    Update GUI.java
    Finished Middle Section of GUI

--- GUI.java
+++ GUI.java
@@ -1,16 +1,16 @@
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import java.awt.GridLayout;
-import javax.swing.JRadioButton;
-import java.awt.Font;
-import javax.swing.JLabel;
 import javax.swing.*;
 import javax.swing.border.*;
+import java.text.*;
+import java.awt.*;
 
 public class GUI 
 {
 
 	public JFrame frame;
+	private JTextField descriptionUpdateTextField;
+	private JTextField dueDateTextField;
+	private JTextField monthUpdateTextField;
+	private JTextField dayUpdateTextField;
 
 
 	public GUI()
@@ -20,23 +20,29 @@ public class GUI
 
 	private void initialize() 
 	{
+		//Create JFrame
 		frame = new JFrame();
 		frame.setBounds(200, 200, 750, 468);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		//First section of GUI Beginning
+		//Create Scrollable List
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(16, 42, 195, 252);
 		frame.getContentPane().add(scrollPane);
 		
+		//List Label
 		JLabel lblTaskList = new JLabel("Task List");
 		lblTaskList.setBounds(79, 14, 61, 16);
 		frame.getContentPane().add(lblTaskList);
 		
-		JPanel panel = new JPanel(new GridLayout(2, 2));
-		panel.setBounds(16, 306, 195, 122);
-		frame.getContentPane().add(panel);
+		//Create panel for sort radio buttons
+		JPanel sortByPanel = new JPanel(new GridLayout(2, 2));
+		sortByPanel.setBounds(16, 306, 195, 122);
+		frame.getContentPane().add(sortByPanel);
 		
+		//Add radio buttons
 		JRadioButton rdbtnDescription = new JRadioButton("Description");
 		rdbtnDescription.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
 		rdbtnDescription.setBounds(57, 310, 111, 23);
@@ -53,16 +59,90 @@ public class GUI
 		rdbtnStatus.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
 		rdbtnStatus.setBounds(57, 387, 141, 23);
 		
-	    Border border = BorderFactory.createTitledBorder("Sort By");
-	    panel.setBorder(border);
+		//Create Radio Button Border
+	    Border sortByPanelBorder = BorderFactory.createTitledBorder("Sort By");
+	    sortByPanel.setBorder(sortByPanelBorder);
 		ButtonGroup group = new ButtonGroup();
 		group.add(rdbtnDescription);
-		panel.add(rdbtnDescription);
+		sortByPanel.add(rdbtnDescription);
 		group.add(rdbtnPriority);
-		panel.add(rdbtnPriority);
+		sortByPanel.add(rdbtnPriority);
 		group.add(rdbtnDueDate);
-		panel.add(rdbtnDueDate);
+		sortByPanel.add(rdbtnDueDate);
 		group.add(rdbtnStatus);
-		panel.add(rdbtnStatus);
+		sortByPanel.add(rdbtnStatus);
+		
+		//Middle Section of the GUI Beginning
+		//Create text fields
+		descriptionUpdateTextField = new JTextField();
+		descriptionUpdateTextField.setBounds(317, 42, 155, 77);
+		frame.getContentPane().add(descriptionUpdateTextField);
+		descriptionUpdateTextField.setColumns(10);
+		
+		//Create Labels
+		JLabel lblDescription = new JLabel("Description:");
+		lblDescription.setBounds(241, 72, 77, 16);
+		frame.getContentPane().add(lblDescription);
+		
+		JLabel lblDueDate = new JLabel("Due Date:");
+		lblDueDate.setBounds(256, 148, 62, 16);
+		frame.getContentPane().add(lblDueDate);
+		
+		JLabel lblMonth = new JLabel("Month");
+		lblMonth.setBounds(327, 125, 40, 16);
+		frame.getContentPane().add(lblMonth);
+		
+		JLabel lblDay = new JLabel("Day");
+		lblDay.setBounds(386, 125, 24, 16);
+		frame.getContentPane().add(lblDay);
+		
+		//Create Month and Day textfields
+		monthUpdateTextField = new JTextField();
+		monthUpdateTextField.setBounds(330, 143, 40, 26);
+		frame.getContentPane().add(monthUpdateTextField);
+		monthUpdateTextField.setColumns(10);
+		
+		dayUpdateTextField = new JTextField();
+		dayUpdateTextField.setColumns(10);
+		dayUpdateTextField.setBounds(379, 143, 40, 26);
+		frame.getContentPane().add(dayUpdateTextField);
+		
+		//Priority Label
+		JLabel lblNewLabel = new JLabel("Priority:");
+		lblNewLabel.setBounds(269, 190, 49, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		//Priority Combo Box
+		JComboBox priorityUpdateComboBox = new JComboBox();
+		
+		//Input number of descriptions using for loop below for combo box options
+		//comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		priorityUpdateComboBox.setBounds(317, 186, 102, 27);
+		frame.getContentPane().add(priorityUpdateComboBox);
+		
+		//Create Buttons
+		JButton btnUpdateTask = new JButton("Update Task");
+		btnUpdateTask.setBounds(317, 271, 123, 29);
+		frame.getContentPane().add(btnUpdateTask);
+		
+		JButton buttonCompleteTask = new JButton("Complete Task");
+		buttonCompleteTask.setBounds(317, 312, 123, 29);
+		frame.getContentPane().add(buttonCompleteTask);
+		
+		JButton buttonDeleteTask = new JButton("Delete Task");
+		buttonDeleteTask.setBounds(317, 353, 123, 29);
+		frame.getContentPane().add(buttonDeleteTask);
+		
+		//Create Status Label
+		JLabel lblStatus = new JLabel("Status:");
+		lblStatus.setBounds(274, 228, 49, 16);
+		frame.getContentPane().add(lblStatus);
+		
+		//Create Status ComboBox with three options
+		JComboBox statusComboBox = new JComboBox();
+		statusComboBox.setModel(new DefaultComboBoxModel(new String[] {"Not Started", "In Progress", "Finished"}));
+		statusComboBox.setBounds(317, 224, 155, 27);
+		frame.getContentPane().add(statusComboBox);
+
 	}
 }
