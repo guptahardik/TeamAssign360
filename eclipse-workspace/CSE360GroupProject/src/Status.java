126203e420124d2d60246777cb4c0a010565990d
sjloucks <46513109+sjloucks@users.noreply.github.com>
2019-04-19 16:23:10 -0700

    implement add, delete, update

--- eclipse-workspace/CSE360GroupProject/src/Status.java
+++ eclipse-workspace/CSE360GroupProject/src/Status.java
@@ -1,12 +1,46 @@
 
+import java.time.LocalDate;
+
 public class Status {
+
 	private int status;
-	private int dateStarted;
-	private int dateFinished;
+	private LocalDate dateStarted;
+	private LocalDate dateFinished;
 	
 	public Status() {
-		status = 1;
-		dateStarted = 0;
-		dateFinished = 0;
+		status = 0;
+		dateStarted = null;
+		dateFinished = null;
+	}
+	
+	public Status(int Stat, LocalDate Date_start, LocalDate Date_end) {
+		status = Stat;
+		dateStarted = Date_start;
+		dateFinished = Date_end;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public void setStatus(int stat) {
+		this.status = stat;
+	}
+	
+	public LocalDate getDateStarted() {
+		return dateStarted;
+	}
+	
+	public void setDateStarted(LocalDate Date_start) {
+		this.dateStarted = Date_start;
+	}
+	
+	public LocalDate getDateFinished() {
+		return dateFinished;
+	}
+	
+	public void setDateFinished(LocalDate Date_end) {
+		this.dateFinished = Date_end;
 	}
 }
+
