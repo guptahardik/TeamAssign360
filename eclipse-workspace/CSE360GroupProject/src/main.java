2fb6ae53b97a18c4675ea8c6a61648a4539c3dbe
sjloucks <46513109+sjloucks@users.noreply.github.com>
2019-04-19 16:21:18 -0700

    create linked list heads

---
+++ eclipse-workspace/CSE360GroupProject/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+	private ToDoList currentHead = null;
+	private ToDoList deletedHead = null;
+	
+	public static void main(String[] args) {
+		GUI window = new GUI();
+		window.frame.setVisible(true);
+	}
+}
